0ffb8d1669e74fdd40abf36ef4819b344bde29bf
Amrit Sundarka <amritsundarka2004@gmail.com>
2024-07-29 12:00:03 +0530

    added vercel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import os from 'os';
 import dotenv from 'dotenv';
-import generateprompts from './prompts.js';
+import generatePrompts from './prompts.js';
 import bodyParser from 'body-parser';
+import callGemini from './gemini.js'
+
 const app = express();
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
@@ -12,14 +14,16 @@ app.get('/', (req, res) => {
     }
 );
 
-app.post('/api/generate-prompts', (req, res) => {
+app.post('/api/generate-prompts', async (req, res) => {
     const { personality, providedText } = req.body;
-    const prompts = generateprompts(personality, providedText);
-    res.json(prompts);
+    const prompts = generatePrompts(personality, providedText);
+    try {
+        const generatedContent = await callGemini(prompts);
+        res.json({ generatedContent });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
